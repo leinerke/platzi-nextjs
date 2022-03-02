d8f9700589c87919782ba99138eeaea512d0d37f
Leinerke Suarez <leinerke2000@gmail.com>
2022-03-02 14:32:14 -0500

    Enlaces.

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from '../component/Navbar';
 
 const About = () => {
   return (
     <div>
+      <Navbar />
       <h1>About</h1>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from '../component/Navbar';
 
 const Home = () => {
   return (
     <div>
+      <Navbar />
       <h1>Hola mundo</h1>
     </div>
   );
