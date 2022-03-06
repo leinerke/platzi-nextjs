2f63731c425c5dcdbf1fa14565dd1835995a2f62
Leinerke Suarez <leinerke2000@gmail.com>
2022-03-06 01:54:16 -0500

    Creando y consumiendo nuestra propia API

---
+++ pages/api/avo/[id].ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import DB from '@database';
+
+const allAvos = async (req: NextApiRequest, res: NextApiResponse) => {
+  const db = new DB();
+  const id = req.query.id;
+  const entry = await db.getById(id as string);
+
+  /*
+  res.statusCode = 200;
+  res.setHeader('Content-type', 'application/json');
+  res.end(JSON.stringify(response));
+   */
+
+  res.status(200).json(entry);
+};
+
+export default allAvos;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,31 @@
-import React from 'react'
-import Navbar from '../components/Navbar/Navbar'
+import React, { useEffect, useState } from 'react';
+import Navbar from '../components/Navbar/Navbar';
 
 const HomePage = () => {
+  const [productList, setProductList] = useState<TProduct[]>([]);
+  const [avo, setAvo] = useState<TProduct>();
+
+  useEffect(() => {
+    fetch('/api/avo')
+      .then(res => res.json())
+      .then(({ data }) => setProductList(data));
+
+    fetch('/api/avo/2zd33b8c')
+      .then(res => res.json())
+      .then(res => setAvo(res));
+  }, []);
+
   return (
     <div>
       <Navbar />
+      {avo && <div>{avo.id}</div>}
       <div>Platzi and Next.js!</div>
-    </div>
-  )
-}
+      {productList.map(product => (
+        <div>{product.name}</div>
+      ))}
 
-export default HomePage
+    </div>
+  );
+};
+
+export default HomePage;
